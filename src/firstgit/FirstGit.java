3ccfb365e7b53298230edd0ac14c2ff41cb60070
mvanweelden1@my.wctc.edu <>
2013-01-23 20:45:04 -0600

    First commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,15 @@
+package firstgit;
+
+/**
+ *
+ * @author Owner
+ */
+public class FirstGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
